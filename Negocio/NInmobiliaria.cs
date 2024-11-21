509fe32a9e435eb532e00e93e2f261121fcb4263
Alejandra Lucia Neyra Yataco <alejandraneyra553@gmail.com>
2024-11-20 23:40:59 -0500

    NInmobiliaria hecho

--- Negocio/NInmobiliaria.cs
+++ Negocio/NInmobiliaria.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Datos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,27 @@ namespace Negocio
 {
     public class NInmobiliaria
     {
+        private DInmobiliaria idInmobiliaria = new DInmobiliaria();
+
+        public string Registrar(Propiedad propiedad)
+        {
+            return idInmobiliaria.Registrar(propiedad);
+        }
+
+        public List<Propiedad> ListarActivos()
+        {
+            return idInmobiliaria.ListarActivos();
+        }
+
+        public string Eliminar(int idPropiedad)
+        {
+            return idInmobiliaria.Eliminar(idPropiedad);
+        }
+
+        public string Modificar(Propiedad propiedad)
+        {
+            return idInmobiliaria.Modificar(propiedad);
+        }
     }
 }
+
