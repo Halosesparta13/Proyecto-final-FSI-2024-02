4d633f517a87f1c3419ef715a0580d57220785f6
Carlos Martinez <u202010153@upc.edu.pe>
2024-11-22 18:49:50 -0500

    Se agrego primer reporte
    Listar usuarios con sus propiedades no eliminadas

--- Presentacion/FormReportes.cs
+++ Presentacion/FormReportes.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Negocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,51 @@ namespace Presentacion
 {
     public partial class FormReportes : Form
     {
+        private NUsuario nUsuario = new NUsuario();
+        private NInmobiliaria nInmobiliaria = new NInmobiliaria();
         public FormReportes()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Limpiar el DataGridView
+            dgvReporte.DataSource = null;
+
+            // Obtener la lista de usuarios
+            var usuarios = nUsuario.ListarTodo();
+
+            // Generar la lista de usuarios y sus propiedades no eliminadas
+            var reporte = new List<dynamic>();
+
+            foreach (var usuario in usuarios)
+            {
+                var propiedades = nInmobiliaria.ListarTodoActivo()
+                                               .FindAll(p => p.IdUsuario == usuario.IdUsuario);
+
+                if (propiedades.Count > 0)
+                {
+                    foreach (var propiedad in propiedades)
+                    {
+                        reporte.Add(new
+                        {
+                            Usuario = usuario.NombreCompleto,
+                            DNI = usuario.DNI,
+                            Correo = usuario.CorreoElectronico,
+                            Propiedad = propiedad.Direccion,
+                            Tipo = propiedad.TipoPropiedad,
+                            Estado = propiedad.Estado
+                        });
+                    }
+                }
+            }
+
+            // Mostrar en el DataGridView
+          
+            dgvReporte.DataSource = reporte;
+
+        }
+    
     }
 }
